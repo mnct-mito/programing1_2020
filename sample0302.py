8087588f2e33df0657ca7b8d0eb1ce53d95fdb19
“mnct-mito” <mito@maizuru-ct.ac.jp>
2020-10-14 18:59:34 +0900

    Create sample0302.py

---
+++ sample0302.py
@@ -0,0 +1,9 @@
+# 第3回　関数定義のサンプルプログラム
+from random import randint
+def today_lunch(menu):
+    lunch = ['しょうが焼き定食', 'ロースカツ定食', 'ラーメンと半炒飯', 'レバニラ炒め定食', 'ナポリタンとハンバーグ定食']
+    index = menu % len(lunch)
+    print('今日のランチは')
+    print(lunch[index])
+menu = randint(0, 5)
+today_lunch(menu)
